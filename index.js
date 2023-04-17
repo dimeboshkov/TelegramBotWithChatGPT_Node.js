84a753a9c5c552ed83842e215f4e995b480cc057
Dime Boshkov <76436987+dimeboshkov@users.noreply.github.com>
2023-04-18 00:31:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
-const { Configuration, OpenAIApi } = require("openai");
-const { Telegraf } = require('telegraf')
-require('dotenv').config()
-
-const bot = new Telegraf("5948449419:AAEvvQWr7HUnEMagkIO-ARym3ImJ1PPBaPM")
-
-bot.start((ctx) => {
-  ctx.reply('Welcome to my ChatGPT bot! Send me a message and I will generate a response for you.')
-})
-
-const configuration = new Configuration({
-  apiKey: "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",
-});
-const openai = new OpenAIApi(configuration);
-
-ctx = "";
-
-bot.on('message', async (ctx) => {
-  try {
-
-    async function runCompletion() {
-      const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: ctx.message.text,
-        max_tokens: 3500,
-        temperature: 0.9,
-      });
-      const user = ctx.from.first_name;
-      console.log(`User ${user} says: ${ctx.message.text}\n`);
-
-      console.log(completion.data.choices[0].text);
-      console.log('____________________________________________________\n \n')
-      ctx.reply(completion.data.choices[0].text)
-
+  const { Configuration, OpenAIApi } = require("openai");
+  const { Telegraf } = require('telegraf')
+  require('dotenv').config()
+  
+  const bot = new Telegraf(process.env['BOT_TOKEN'])
+  
+  bot.start((ctx) => {
+    ctx.reply('Welcome to my ChatGPT bot! Send me a message and I will generate a response for you.')
+  })
+  
+  const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+  const openai = new OpenAIApi(configuration);
+  
+  ctx = "";
+  
+  bot.on('message', async (ctx) => {
+    try {
+  
+      async function runCompletion() {
+        const completion = await openai.createCompletion({
+          model: "text-davinci-003",
+          prompt: ctx.message.text,
+          max_tokens: 3500,
+          temperature: 0.9,
+        });
+        const user = ctx.from.first_name;
+        console.log(`User ${user} says: ${ctx.message.text}\n`);
+  
+        //console.log(openai.chatid)
+        console.log(completion.data.choices[0].text);
+        console.log('____________________________________________________\n \n')
+        ctx.reply(completion.data.choices[0].text)
+  
+      }
+  
+      runCompletion();
+  
+    } catch (error) {
+      console.log(error)
     }
-
-    runCompletion();
-
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-bot.launch()
+  })
+  
+  
+  bot.launch()
+  
